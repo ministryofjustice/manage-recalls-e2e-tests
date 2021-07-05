d1cc3794d307505ac90bea79204095c2e3220001
Jon Wyatt <>
2021-07-05 17:11:56 +0100

    Use button to confirm offender profile page loaded

--- src/test/java/cucumber/steps/NavigationSteps.java
+++ src/test/java/cucumber/steps/NavigationSteps.java
@@ -161,7 +161,7 @@ public class NavigationSteps {
     @Then("{word} is on the Offender profile page")
     public void onOffenderProfilePage(String customer) {
         theActorCalled(customer).attemptsTo(
-                Ensure.thatTheCurrentPage().title().hasValue().isEqualTo(OffenderProfilePage.TITLE)
+                Ensure.that(OffenderProfilePage.CREATE_RECALL_BUTTON).hasTextContent('Create recall')
         );
     }
 
