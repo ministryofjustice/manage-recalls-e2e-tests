0d40697f0ec5404adb22559cd2fd1d49df90337a
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-07-22 15:31:00 +0100

    PUD-276:  Update spelling of licence

--- src/test/java/cucumber/pages/PpudPage.java
+++ src/test/java/cucumber/pages/PpudPage.java
@@ -5,7 +5,7 @@ import net.thucydides.core.pages.PageObject;
 
 public abstract class PpudPage extends PageObject {
 
-    protected static final String TITLE_ROOT = "Manage a recall for an offender on license - ";
+    protected static final String TITLE_ROOT = "Manage a recall for an offender on licence - ";
 
     protected static Target getTargetByDataQa(String dataQa) {
         return Target.the(dataQa + " matches").locatedBy("[data-qa='" + dataQa + "']");
