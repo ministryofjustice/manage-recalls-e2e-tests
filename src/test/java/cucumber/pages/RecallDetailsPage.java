2a2892827d100845db4fcce2554a420d78b7c349
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@digital.justice.gov.uk>
2021-08-16 17:24:06 +0100

    PUD-133|SR|capture vulnerabilities and contraband details (#32)
    * PUD-133|SR|capture vulnerabiliites and contraband details
    
    * modify element idendifier to user id instead of xpath

--- src/test/java/cucumber/pages/RecallDetailsPage.java
+++ src/test/java/cucumber/pages/RecallDetailsPage.java
@@ -9,6 +9,9 @@ public class RecallDetailsPage extends PpudPage {
     public static final Target RELEASING_PRISON = getTargetByDataQa("lastReleasePrison");
     public static final Target LAST_RELEASE_DATE = getTargetByDataQa("lastReleaseDateTime");
     public static final Target LOCAL_POLICE_STATION = getTargetByDataQa("localPoliceService");
+    public static final Target VULNERABILITY_DIVERSITY_DETAIL = getTargetByDataQa("vulnerabilityDiversityDetail");
+    public static final Target CONTRABAND_DETAIL = getTargetByDataQa("contrabandDetail");
+    public static final Target MAPPA_LEVEL = getTargetByDataQa("mappaLevel");
     public static final Target RECALL_DOCUMENT_LINK_PART_A = getTargetByDataQa("uploadedDocument-PART_A_RECALL_REPORT");
     public static final Target RECALL_DOCUMENT_LINK_LICENCE = getTargetByDataQa("uploadedDocument-LICENCE");
 }
