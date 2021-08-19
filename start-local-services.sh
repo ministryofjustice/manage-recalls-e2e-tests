871720680279b707e19e3ba61cf09d2cb9c73c59
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-08-19 09:50:31 +0100

    Rename build-local.sh back to build.sh to be consistent with ui and api.  Increase timeout when checking api and ui are running in start-local-services.sh.  Ensure ui and api are not running before trying to run again

--- start-local-services.sh
+++ start-local-services.sh
@@ -14,8 +14,10 @@ readonly LOCAL_DOCKER_COMPOSE_FILE=docker-compose.yml
 docker compose -f $LOCAL_DOCKER_COMPOSE_FILE pull redis gotenberg hmpps-auth fake-prisoner-offender-search-api postgres localstack
 docker compose -f $LOCAL_DOCKER_COMPOSE_FILE up redis gotenberg hmpps-auth fake-prisoner-offender-search-api postgres localstack --remove-orphans -d
 
+npx kill-port 3000 8080
+
 pushd ${MANAGE_RECALLS_UI_DIR}
-npm run build
+npm run clean build
 npm run start:e2e >> "${MANAGE_RECALLS_UI_LOG_FILE}" 2>&1 &
 popd
 
@@ -28,10 +30,10 @@ docker run --network container:hmpps-auth-e2e \
     appropriate/curl -s -4 --retry 120 --retry-delay 1 --retry-connrefused http://localhost:8080/auth/health/ping
 
 printf "\nChecking ${MANAGE_RECALLS_API_NAME} is running..."
-curl -s -4 --retry 10 --retry-delay 1 --retry-connrefused http://localhost:8080/health/ping
+curl -s -4 --retry 20 --retry-delay 1 --retry-connrefused http://localhost:8080/health/ping
 
 printf "\nChecking ${MANAGE_RECALLS_UI_NAME} is running..."
-curl -s -4 --retry 10 --retry-delay 1 --retry-connrefused http://localhost:3000/ping
+curl -s -4 --retry 20 --retry-delay 1 --retry-connrefused http://localhost:3000/ping
 
 echo "Logs can be found by running:"
 echo "  less ${MANAGE_RECALLS_API_LOG_FILE}"
