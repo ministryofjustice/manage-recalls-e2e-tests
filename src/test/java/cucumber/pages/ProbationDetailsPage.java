d3a154fda456c3af3e30b56d8b9ae89bed7ff996
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@digital.justice.gov.uk>
2021-08-19 15:12:50 +0100

    PUD-131|SR|add probation details

--- src/test/java/cucumber/pages/ProbationDetailsPage.java
+++ src/test/java/cucumber/pages/ProbationDetailsPage.java
@@ -8,6 +8,6 @@ public class ProbationDetailsPage extends PpudPage{
     public static final Target PROBATION_OFFICER_NAME_FIELD = Target.the("probation officer name field").located(By.id("probationOfficerName"));
     public static final Target PROBATION_OFFICER_PHONE_NO_FIELD = Target.the("probation officer phone number field").located(By.id("probationOfficerPhoneNumber"));
     public static final Target PROBATION_OFFICER_EMAIL_FIELD = Target.the("probation officer email field").located(By.id("probationOfficerEmail"));
-    public static final Target PROBATION_DIVISION_FIELD = Target.the("probation division field").located(By.id("probationDivision"));
+    public static final Target PROBATION_DIVISION_DROPDOWN = Target.the("probation division field").located(By.id("probationDivision"));
     public static final Target ASSISTANT_CHIEF_OFFICER_NAME_FIELD = Target.the("assistant chief officer name field").located(By.id("authorisingAssistantChiefOfficer"));
 }
--- src/test/java/cucumber/pages/RecallReceivedPage.java
+++ src/test/java/cucumber/pages/RecallReceivedPage.java
@@ -1,14 +1,15 @@
 package cucumber.pages;
 
 import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
 
 public class RecallReceivedPage extends PpudPage {
 
     public static final String TITLE = TITLE_ROOT + "Book a recall - When did you receive the recall request?";
-    public static final Target DAY_FIELD = getTargetByName("day");
-    public static final Target MONTH_FIELD = getTargetByName("month");
-    public static final Target YEAR_FIELD = getTargetByName("year");
-    public static final Target HOUR_FIELD = getTargetByName("hour");
-    public static final Target MINUTE_FIELD = getTargetByName("minute");
+    public static final Target DAY_FIELD = Target.the("recall received day field").located(By.id("recallEmailReceivedDateTime-recallEmailReceivedDateTimeDay"));
+    public static final Target MONTH_FIELD = Target.the("recall received month field").located(By.id("recallEmailReceivedDateTime-recallEmailReceivedDateTimeMonth"));
+    public static final Target YEAR_FIELD = Target.the("recall received year field").located(By.id("recallEmailReceivedDateTime-recallEmailReceivedDateTimeYear"));
+    public static final Target HOUR_FIELD = Target.the("recall received hour field").located(By.id("recallEmailReceivedDateTime-recallEmailReceivedDateTimeHour"));
+    public static final Target MINUTE_FIELD = Target.the("recall received minute field").located(By.id("recallEmailReceivedDateTime-recallEmailReceivedDateTimeMinute"));
 
 }
