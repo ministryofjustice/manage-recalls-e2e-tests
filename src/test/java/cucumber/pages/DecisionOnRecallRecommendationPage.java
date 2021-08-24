52ccc619890cd27b45edca700e1b49329966abdb
Phil Segal <phil@phisec.co.uk>
2021-08-24 11:06:32 +0100

    PUD-144: Assert on recall length - clean up steps (use a single attemptsTo and merge dependent steps)

--- src/test/java/cucumber/pages/DecisionOnRecallRecommendationPage.java
+++ src/test/java/cucumber/pages/DecisionOnRecallRecommendationPage.java
@@ -6,7 +6,7 @@ public class DecisionOnRecallRecommendationPage extends PpudPage {
 
     public static final String TITLE = TITLE_ROOT + "Decision on recall recommendation";
     public static final Target CONTINUE_BUTTON = getTargetByDataQa("continueButton");
-    public static final Target RECALL_LENGTH_14_DAYS = getTargetByInputValue("YES");
-    public static final Target QUESTION_AROUND_RECALL_LENGTH =  Target.the("Do you agree with the recommended recall length of 14 days").locatedBy("//*[@id=\"main-content\"]/div/div/form/div[1]/fieldset/legend");
+    public static final Target YES_RADIO = getTargetByInputValue("YES");
+    public static final Target QUESTION_AROUND_RECALL_LENGTH = getTargetByDataQa("recommended-recall-length-text");
 
 }
