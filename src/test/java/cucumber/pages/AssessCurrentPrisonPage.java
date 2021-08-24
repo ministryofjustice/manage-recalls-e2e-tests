b88cce09eb79069f7ab3ac82a5466e001dc3ae74
Jon Wyatt <jonnywyatt2@gmail.com>
2021-08-24 12:34:07 +0100

    PUD-137 - Extend assess recall test for 'current prison' (#41)
    Co-authored-by: Jon Wyatt <>

---
+++ src/test/java/cucumber/pages/AssessCurrentPrisonPage.java
@@ -0,0 +1,9 @@
+package cucumber.pages;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class AssessCurrentPrisonPage extends PpudPage {
+
+    public static final Target CURRENT_PRISON = getTargetByName("currentPrison");
+
+}
--- src/test/java/cucumber/pages/PoliceContactDetailsPage.java
+++ src/test/java/cucumber/pages/PoliceContactDetailsPage.java
@@ -3,6 +3,6 @@ package cucumber.pages;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class PoliceContactDetailsPage extends PpudPage {
-    public static final String TITLE = TITLE_ROOT + "Book a recall - What are the police contact details?";
+    public static final String TITLE = TITLE_ROOT + "Book a recall - What is the local police force?";
     public static final Target LOCAL_POLICE_FORCE = getTargetByName("localPoliceForce");
 }
