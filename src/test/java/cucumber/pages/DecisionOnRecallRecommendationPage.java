e79e9477f4f101365b341be8b0a526926e52e50b
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@digital.justice.gov.uk>
2021-08-27 15:17:42 +0100

    PUD-87|SR|capture-decision-context (#45)

--- src/test/java/cucumber/pages/DecisionOnRecallRecommendationPage.java
+++ src/test/java/cucumber/pages/DecisionOnRecallRecommendationPage.java
@@ -1,12 +1,14 @@
 package cucumber.pages;
 
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class DecisionOnRecallRecommendationPage extends PpudPage {
 
     public static final String TITLE = TITLE_ROOT + "Decision on recall recommendation";
     public static final Target CONTINUE_BUTTON = getTargetByDataQa("continueButton");
-    public static final Target YES_RADIO = getTargetByInputValue("YES");
+    public static final Target YES_RADIO = Target.the("radio button to proceed with recommended fixed recall").located(By.id("agreeWithRecall"));
     public static final Target QUESTION_AROUND_RECALL_LENGTH = getTargetByDataQa("recommended-recall-length-text");
+    public static final Target MORE_DETAILS_FOR_YES_OPTION_TEXT_BOX = Target.the("text box to provide more details to proceed with recommended fixed recall").located(By.id("agreeWithRecallDetailYes"));
 
 }
