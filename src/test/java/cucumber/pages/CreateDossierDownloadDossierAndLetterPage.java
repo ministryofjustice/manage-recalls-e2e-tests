b2946f47e12d11badc25d26295a978dd2c148e9a
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@digital.justice.gov.uk>
2021-09-06 14:26:20 +0100

    Pud 122 record issuance of recall notification (#60)
    * PUD-122-record-issuance-of-recall-notification-part1
    
    * remove commented out code
    
    * PUD-122-record-issuance-of-recall-notifcation-part-2

---
+++ src/test/java/cucumber/pages/CreateDossierDownloadDossierAndLetterPage.java
@@ -0,0 +1,7 @@
+package cucumber.pages;
+
+public class CreateDossierDownloadDossierAndLetterPage extends PpudPage {
+
+    public static final String TITLE = TITLE_ROOT + "Download the dossier and letter";
+
+}
--- src/test/java/cucumber/pages/RecordIssuanceOfRecallNotificationPage.java
+++ src/test/java/cucumber/pages/RecordIssuanceOfRecallNotificationPage.java
@@ -7,10 +7,11 @@ public class RecordIssuanceOfRecallNotificationPage extends PpudPage {
 
     public static final String TITLE = TITLE_ROOT + "Assess a recall - Email the recall notification";
     public static final Target CONFIRM_RECALL_NOTIFICATION_EMAIL_SENT_CHECKBOX = Target.the("checkbox to confirm that an email has been sent for the recall notification").located(By.id("confirmRecallNotificationEmailSent"));
-    public static final Target DAY_FIELD = Target.the("recall notification email sent - day field").located(org.openqa.selenium.By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeDay"));
-    public static final Target MONTH_FIELD = Target.the("recall notification email sent - month field").located(org.openqa.selenium.By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeMonth"));
-    public static final Target YEAR_FIELD = Target.the("recall notification email sent - year field").located(org.openqa.selenium.By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeYear"));
-    public static final Target HOUR_FIELD = Target.the("recall notification email sent - hour field").located(org.openqa.selenium.By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeHour"));
-    public static final Target MINUTE_FIELD = Target.the("recall notification email sent - minute field").located(org.openqa.selenium.By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeMinute"));
+    public static final Target DAY_FIELD = Target.the("recall notification email sent - day field").located(By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeDay"));
+    public static final Target MONTH_FIELD = Target.the("recall notification email sent - month field").located(By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeMonth"));
+    public static final Target YEAR_FIELD = Target.the("recall notification email sent - year field").located(By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeYear"));
+    public static final Target HOUR_FIELD = Target.the("recall notification email sent - hour field").located(By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeHour"));
+    public static final Target MINUTE_FIELD = Target.the("recall notification email sent - minute field").located(By.id("recallNotificationEmailSentDateTime-recallNotificationEmailSentDateTimeMinute"));
+    public static final Target UPLOAD_FILE = Target.the("upload file").located(org.openqa.selenium.By.id("recallNotificationEmailFileName"));
 
 }
