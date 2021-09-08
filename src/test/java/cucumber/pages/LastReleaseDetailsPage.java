cc10d7e82fe6714535857451f4eec31b3b973ad5
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@digital.justice.gov.uk>
2021-09-08 11:52:15 +0100

    PUD-492|SR|add inactive prison to releasing prison

--- src/test/java/cucumber/pages/LastReleaseDetailsPage.java
+++ src/test/java/cucumber/pages/LastReleaseDetailsPage.java
@@ -8,7 +8,7 @@ public class LastReleaseDetailsPage extends PpudPage {
     public static final Target RELEASING_PRISON_AUTOCOMPLETE_FIELD = Target.the("releasing prison dropdown field").located(By.id("lastReleasePrison"));
 
     public static Target getTargetForReleasingPrison(String prison){
-        return Target.the("releasing prison, Ashfield (HMP)").located(By.xpath("//ul[@id=\"lastReleasePrison__listbox\"]/li[contains(text(), \"" + prison +"\")]"));
+        return Target.the("releasing prison, Ashwell (HMP)").located(By.xpath("//ul[@id=\"lastReleasePrison__listbox\"]/li[contains(text(), \"" + prison +"\")]"));
     }
 
 }
