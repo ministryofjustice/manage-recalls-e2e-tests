7aecb54589ae1e36c198af98b4bc291167a4c74a
Jon Wyatt <jonnywyatt2@gmail.com>
2021-09-13 14:38:09 +0100

    Pre-cons main name (#69)
    Co-authored-by: Jon Wyatt <>

---
+++ src/test/java/cucumber/pages/RecallPreConsNamePage.java
@@ -0,0 +1,10 @@
+package cucumber.pages;
+
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class RecallPreConsNamePage extends PpudPage {
+    public static final Target HAS_OTHER_PRE_CONS_NAME_OPTION = Target.the("pre-cons main name option - other").located(By.id("hasOtherPreviousConvictionMainName-2"));
+    public static final Target OTHER_PRE_CONS_NAME = Target.the("pre-cons main name text box").located(By.id("previousConvictionMainName"));
+
+}
--- src/test/resources/features/managerecalls/ManageRecall.feature
+++ src/test/resources/features/managerecalls/ManageRecall.feature
@@ -6,6 +6,7 @@ Feature: Manage Recalls
     When Maria searches for the environment specific NOMS number
     ### Book a recall
     And Maria clicks on the Book a recall link
+    And Maria enters the pre-convictions name
     And Maria submits the date and time of the recall request received from probation service
     And Maria submits the sentence, offence and release details
     And Maria submits the police contact details
