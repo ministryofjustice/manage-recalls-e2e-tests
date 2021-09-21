d9a0ade0a7ed5f04f13e71b6f92555fa814d8c46
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-09-21 12:37:37 +0100

    PUD-540: Delete unused constants

--- src/test/java/cucumber/pages/AssessARecallPage.java
+++ src/test/java/cucumber/pages/AssessARecallPage.java
@@ -14,49 +14,6 @@ import net.thucydides.core.annotations.NamedUrls;
 public class AssessARecallPage extends PpudPage {
     public static final String TITLE = TITLE_ROOT + "Assess a recall";
 
-    public class PersonDetails {
-        public static final Target NAME = getTargetByDataQa("name");
-        public static final Target PRE_CONS_NAME = getTargetByDataQa("previousConvictionMainName");
-        public static final Target DATE_OF_BIRTH = getTargetByDataQa("dateOfBirth");
-        public static final Target GENDER = getTargetByDataQa("gender");
-        public static final Target CRO_NUMBER = getTargetByDataQa("croNumber");
-        public static final Target PNC_NUMBER = getTargetByDataQa("pncNumber");
-        public static final Target NOMS_NUMBER = getTargetByDataQa("nomsNumber");
-    }
-    public class RecallDetails {
-        public static final Target RECALL_LENGTH = getTargetByDataQa("recallLength");
-        public static final Target DATE_RECALL_EMAIL_RECEIVED = getTargetByDataQa("recallEmailReceivedDateTime");
-        public static final Target LOCAL_POLICE_STATION = getTargetByDataQa("localPoliceForce");
-    }
-    public class IssuesAndNeedsDetails {
-        public static final Target VULNERABILITY_DIVERSITY_DETAIL = getTargetByDataQa("vulnerabilityDiversityDetail");
-        public static final Target CONTRABAND_DETAIL = getTargetByDataQa("contrabandDetail");
-        public static final Target MAPPA_LEVEL = getTargetByDataQa("mappaLevel");
-    }
-    public class SentenceOffenceAndReleaseDetails {
-        public static final Target SENTENCE_DATE = getTargetByDataQa("sentenceDate");
-        public static final Target SENTENCE_EXPIRY_DATE = getTargetByDataQa("sentenceExpiryDate");
-        public static final Target LICENCE_EXPIRY_DATE = getTargetByDataQa("licenceExpiryDate");
-        public static final Target CONDITIONAL_RELEASE_DATE = getTargetByDataQa("conditionalReleaseDate");
-        public static final Target LAST_RELEASE_PRISON = getTargetByDataQa("lastReleasePrison");
-        public static final Target LAST_RELEASE_DATE = getTargetByDataQa("lastReleaseDate");
-        public static final Target SENTENCE_LENGTH = getTargetByDataQa("sentenceLength");
-        public static final Target INDEX_OFFENCE = getTargetByDataQa("indexOffence");
-        public static final Target BOOKING_NUMBER = getTargetByDataQa("bookingNumber");
-    }
-    public class ProbationDetails {
-        public static final Target PROBATION_OFFICER_NAME = getTargetByDataQa("probationOfficerName");
-        public static final Target PROBATION_OFFICER_PHONE_NO = getTargetByDataQa("probationOfficerPhoneNumber");
-        public static final Target PROBATION_OFFICER_EMAIL = getTargetByDataQa("probationOfficerEmail");
-        public static final Target PROBATION_DIVISION = getTargetByDataQa("probationDivision");
-        public static final Target ASSISTANT_CHIEF_OFFICER_NAME = getTargetByDataQa("authorisingAssistantChiefOfficer");
-    }
-    public class DocumentDetails {
-        public static final Target RECALL_DOCUMENT_LINK_PART_A = getTargetByDataQa("uploadedDocument-PART_A_RECALL_REPORT");
-        public static final Target RECALL_DOCUMENT_LINK_LICENCE = getTargetByDataQa("uploadedDocument-LICENCE");
-        public static final Target RECALL_DOCUMENT_LINK_PREVIOUS_CONVICTIONS_SHEET = getTargetByDataQa("uploadedDocument-PREVIOUS_CONVICTIONS_SHEET");
-        public static final Target RECALL_DOCUMENT_LINK_PRE_SENTENCING_REPORT = getTargetByDataQa("uploadedDocument-PRE_SENTENCING_REPORT");
-    }
     public class RecallAssessmentDetails {
         public static final Target ASSESSED_BY_USERNAME = getTargetByDataQa("assessedByUserName");
         public static final Target AGREE_WITH_RECALL_RECOMMENDATION = getTargetByDataQa("agreeWithRecall");
@@ -69,6 +26,7 @@ public class AssessARecallPage extends PpudPage {
         public static final Target DATETIME_RECALL_NOTIFICATION_EMAIL_SENT = getTargetByDataQa("recallNotificationEmailSentDateTime");
         public static final Target UPLOADED_RECALL_NOTIFICATION_EMAIL_LINK = getTargetByDataQa("uploadedDocument-RECALL_NOTIFICATION_EMAIL");
     }
+
     public class CreateDossierDetails {
         public static final Target ADDITIONAL_LICENCE_CONDITIONS = getTargetByDataQa("additionalLicenceConditions");
         public static final Target MORE_DETAILS_FOR_ADDITIONAL_LICENCE_CONDITIONS_TEXT = getTargetByDataQa("additionalLicenceConditionsDetail");
