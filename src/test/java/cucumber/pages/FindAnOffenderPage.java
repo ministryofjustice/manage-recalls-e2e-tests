8b9bde611be9be92ac0a5a67a67049d916b41b8d
Jon Wyatt <jonnywyatt2@gmail.com>
2021-09-29 11:57:43 +0100

    Update find person input selector (#84)
    Co-authored-by: Jon Wyatt <>

--- src/test/java/cucumber/pages/FindAnOffenderPage.java
+++ src/test/java/cucumber/pages/FindAnOffenderPage.java
@@ -8,7 +8,7 @@ public class FindAnOffenderPage extends PpudPage {
 
     public static final String TITLE = TITLE_ROOT + "Home";
 
-    public static final Target NOMS_NUMBER_INPUT = Target.the("nomsNumber input").locatedBy("#search-field");
+    public static final Target NOMS_NUMBER_INPUT = Target.the("nomsNumber input").locatedBy("[data-qa='search-field']");
     public static final Target SEARCH_BUTTON = Target.the("search button").locatedBy("[data-qa='search'][type='submit']");
 
     public static final Target SEARCH_RESULT_TEXT = getTargetByDataQa("search-results-count");
