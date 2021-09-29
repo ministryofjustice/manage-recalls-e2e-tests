122ce26d304554b08ad088958cd1bc535b2a53ad
Phil Segal <phil@phisec.co.uk>
2021-09-29 14:39:15 +0100

    PUD-558: Ensure test fails if Internal error when opening pdf. - only take screenshots on failures - take full page screenshots - run without headless flag as there is a bug in chromedriver when getting current url of a 2nd tab - Use screenplay for file upload

--- src/test/java/cucumber/pages/PpudPage.java
+++ src/test/java/cucumber/pages/PpudPage.java
@@ -1,10 +1,15 @@
 package cucumber.pages;
 
+import com.assertthat.selenium_shutterbug.utils.web.Browser;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Upload;
 import net.serenitybdd.screenplay.targets.Target;
 import net.thucydides.core.pages.PageObject;
 import net.thucydides.core.pages.components.FileToUpload;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.nio.file.Path;
 
 public abstract class PpudPage extends PageObject {
     protected static final String TITLE_ROOT = "Manage a recall - ";
@@ -35,9 +40,4 @@ public abstract class PpudPage extends PageObject {
         return Target.the(fieldsetName + " matches").located(By.name(fieldsetName + "Day"));
     }
 
-    public void uploadFile(String filePath, String elementName) {
-        WebDriver driver = getDriver();
-        FileToUpload fileToUpload = new FileToUpload(driver, filePath);
-        fileToUpload.fromLocalMachine().to(find(By.name(elementName)));
-    }
 }
