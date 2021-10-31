3d743e58dcc9e16305d07f4b1383ad0bfcfc71cd
Jon Wyatt <>
2021-10-31 13:23:15 +0000

    Update page titles

--- src/test/java/cucumber/pages/FindAnOffenderPage.java
+++ src/test/java/cucumber/pages/FindAnOffenderPage.java
@@ -6,7 +6,7 @@ import net.thucydides.core.annotations.DefaultUrl;
 @DefaultUrl("https://manage-recalls-dev.hmpps.service.justice.gov.uk/find-person")
 public class FindAnOffenderPage extends PpudPage {
 
-    public static final String TITLE = TITLE_ROOT + "Home";
+    public static final String TITLE = TITLE_ROOT + "Find a person";
 
     public static final Target NOMS_NUMBER_INPUT = Target.the("nomsNumber input").locatedBy("[data-qa='search-field']");
     public static final Target SEARCH_BUTTON = Target.the("search button").locatedBy("[data-qa='search'][type='submit']");
