5ca8ac6a7c4521ccd382c41272959d960ccc2de0
Jon Wyatt <jonnywyatt2@gmail.com>
2021-11-15 09:19:41 +0000

    Check revocation order download; remove delete doc step to prevent flakiness (#116)
    * Check revocation order download; remove delete doc step to prevent flakiness
    
    * Increase timeouts
    
    * Restore deletion, increase timeouts
    
    * Revert "Restore deletion, increase timeouts"
    
    This reverts commit fb95cae897c007e6dd790801987dd6b0bf2cb40b.
    
    Co-authored-by: Jon Wyatt <>

--- src/test/java/cucumber/steps/NavigationSteps.java
+++ src/test/java/cucumber/steps/NavigationSteps.java
@@ -246,14 +246,10 @@ public class NavigationSteps {
         Path testPdfPath = Path.of("src/test/resources/files/test.pdf");
         Path licencePdfPath = Path.of("src/test/resources/files/Licence.pdf");
         theActorCalled(customer).attemptsTo(
-                Upload.theFile(testPdfPath).to(UploadRecallDocumentsPage.DOCUMENT_UPLOAD),
-                Wait.until(WebElementQuestion.the(UploadRecallDocumentsPage.getTargetForCategoryDropdownByNumber("UNCATEGORISED", 0)), WebElementStateMatchers.isVisible()).forNoLongerThan(2).seconds(),
-                Click.on(UploadRecallDocumentsPage.DELETE_FIRST_DOCUMENT_BY_LINK),
-                Wait.until(WebElementQuestion.the(UploadRecallDocumentsPage.waitForNumberOfDocuments("0")), WebElementStateMatchers.isVisible()).forNoLongerThan(2).seconds(),
                 Upload.theFile(licencePdfPath).to(UploadRecallDocumentsPage.DOCUMENT_UPLOAD),
-                Wait.until(WebElementQuestion.the(UploadRecallDocumentsPage.getTargetForCategoryDropdownByNumber("LICENCE", 0)), WebElementStateMatchers.isVisible()).forNoLongerThan(2).seconds(),
+                Wait.until(WebElementQuestion.the(UploadRecallDocumentsPage.getTargetForCategoryDropdownByNumber("LICENCE", 0)), WebElementStateMatchers.isVisible()).forNoLongerThan(10).seconds(),
                 Upload.theFile(testPdfPath).to(UploadRecallDocumentsPage.DOCUMENT_UPLOAD),
-                Wait.until(WebElementQuestion.the(UploadRecallDocumentsPage.getTargetForCategoryDropdownByNumber("UNCATEGORISED", 1)), WebElementStateMatchers.isVisible()).forNoLongerThan(2).seconds(),
+                Wait.until(WebElementQuestion.the(UploadRecallDocumentsPage.getTargetForCategoryDropdownByNumber("UNCATEGORISED", 1)), WebElementStateMatchers.isVisible()).forNoLongerThan(10).seconds(),
                 SelectFromOptions.byValue("PART_A_RECALL_REPORT").from(UploadRecallDocumentsPage.getTargetForCategoryDropdownByNumber("UNCATEGORISED", 1)),
                 Click.on(UploadRecallDocumentsPage.CONTINUE_BUTTON)
         );
@@ -482,6 +478,7 @@ public class NavigationSteps {
 
     @Then("{word} is able to see the recall information before creating a dossier")
     public void confirmRecallDetailsBeforeDossier(String customer) {
+        openDocumentInTab(customer, CreateDossierDetails.REVOCATION_ORDER_DOCUMENT_LINK);
         theActorCalled(customer).attemptsTo(
                 Ensure.that(CreateDossierDetails.DOSSIER_DUE_DATE).text().isEqualTo("Overdue: Due on 7 December 2020"),
                 Ensure.that(RecallAssessmentDetails.CURRENT_PRISON).text().isEqualTo("Ashfield (HMP)"),
