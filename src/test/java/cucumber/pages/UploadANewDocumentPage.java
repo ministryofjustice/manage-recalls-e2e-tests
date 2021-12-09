9b5a786f0cd679d789bf9348aeb34d52059ec278
Jon Wyatt <jonnywyatt2@gmail.com>
2021-12-09 08:57:50 +0000

    Update field name for new doc version page (#134)
    Co-authored-by: Jon Wyatt <>

--- src/test/java/cucumber/pages/UploadANewDocumentPage.java
+++ src/test/java/cucumber/pages/UploadANewDocumentPage.java
@@ -5,6 +5,6 @@ import org.openqa.selenium.By;
 
 public class UploadANewDocumentPage extends PpudPage {
     public static final String TITLE = TITLE_ROOT + "Book a recall - Upload a new document";
-    public static final Target DOCUMENT_UPLOAD = Target.the("upload element").located(By.name("documents"));
+    public static final Target DOCUMENT_UPLOAD = Target.the("upload element").located(By.name("document"));
 
 }
