012ba01a0d1dad92e97b14454ff51ee376607daa
Jon Wyatt <jonnywyatt2@gmail.com>
2021-12-10 14:40:26 +0000

    PUD-1071 - Change history page - confirm who uploaded the doc (#136)
    * Change history page
    
    * Remove unused var
    
    Co-authored-by: Jon Wyatt <>

---
+++ src/test/java/cucumber/pages/ChangeHistoryPage.java
@@ -0,0 +1,11 @@
+package cucumber.pages;
+
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.screenplay.targets.Target;
+
+public class ChangeHistoryPage extends PpudPage {
+
+    public static final String TITLE = TITLE_ROOT + "Change history";
+    public static final Target UPLOADED_PART_A_USER = getTargetBySelector("[data-qa='PART_A_RECALL_REPORT'] [data-qa='createdByUserName']");
+    public static final Target UPLOADED_PART_A_HISTORY_LINK = getTargetByDataQa("viewHistory-PART_A_RECALL_REPORT");
+}
