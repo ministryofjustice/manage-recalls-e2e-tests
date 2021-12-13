2bcd66d70c66bf2bb49e145feb18a2694a9147dc
Jon Wyatt <>
2021-12-13 12:26:39 +0000

    Navigate to document history page

---
+++ src/test/java/cucumber/pages/ChangeHistoryDocumentCategoryPage.java
@@ -0,0 +1,9 @@
+package cucumber.pages;
+
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.screenplay.targets.Target;
+
+public class ChangeHistoryDocumentCategoryPage extends PpudPage {
+
+    public static final String TITLE = TITLE_ROOT + "Uploaded document change history";
+}
