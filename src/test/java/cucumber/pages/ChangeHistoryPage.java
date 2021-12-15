412860d074e68d8f197edf008212229079270236
Jon Wyatt <>
2021-12-15 17:48:41 +0000

    Check generated dossier link is present

--- src/test/java/cucumber/pages/ChangeHistoryPage.java
+++ src/test/java/cucumber/pages/ChangeHistoryPage.java
@@ -8,4 +8,5 @@ public class ChangeHistoryPage extends PpudPage {
     public static final String TITLE = TITLE_ROOT + "Change history";
     public static final Target UPLOADED_PART_A_USER = getTargetBySelector("[data-qa='PART_A_RECALL_REPORT'] [data-qa='createdByUserName']");
     public static final Target UPLOADED_PART_A_HISTORY_LINK = getTargetByDataQa("viewHistory-PART_A_RECALL_REPORT");
+    public static final Target GENERATED_DOSSIER_LINK = getTargetByDataQa("generatedDocument-DOSSIER");
 }
