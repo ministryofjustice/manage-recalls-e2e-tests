38e885121e1d3d59cc25b8741b4fa0ec2a2d645c
Jon Wyatt <>
2021-12-17 12:39:54 +0000

    Enter details for new document version

--- src/test/java/cucumber/pages/UploadANewDocumentPage.java
+++ src/test/java/cucumber/pages/UploadANewDocumentPage.java
@@ -6,5 +6,6 @@ import org.openqa.selenium.By;
 public class UploadANewDocumentPage extends PpudPage {
     public static final String TITLE = TITLE_ROOT + "Book a recall - Upload a new document";
     public static final Target DOCUMENT_UPLOAD = Target.the("upload element").located(By.name("document"));
+    public static final Target DETAILS = Target.the("details element").located(By.name("details"));
 
 }
