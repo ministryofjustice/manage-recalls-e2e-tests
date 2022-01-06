9e26cb0844b59cbe3a41def7fe40d7e4e31db7f5
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-01-06 12:56:47 +0000

    Check auth is ok before running tests

--- ci-local-mode-docker/run-tests.sh
+++ ci-local-mode-docker/run-tests.sh
@@ -2,18 +2,24 @@
 
 set -euo pipefail
 
-manage_recalls_url=http://manage-recalls-ui:3000
+function wait_for() {
+  echo "Waiting for ${1} to be available..."
+  until $(curl --output /dev/null --silent --head --fail ${2}); do
+    echo '.'
+    sleep 5
+  done
+}
 
-echo "Waiting for manage-recalls-ui to be available..."
-until $(curl --output /dev/null --silent --head --fail $manage_recalls_url); do
-  printf '.'
-  sleep 5
-done
+manage_recalls_ui_url=http://manage-recalls-ui:3000/ping
+hmpps_auth_url=http://hmpps-auth:8080/auth/health
+
+wait_for "manage-recalls-ui" "${manage_recalls_ui_url}"
+wait_for "hmpps-auth" "${hmpps_auth_url}"
 
 sleep 2
 
 cypress run \
-  --config baseUrl=$manage_recalls_url \
+  --config baseUrl=${manage_recalls_ui_url} \
   --browser chrome \
   --record \
   --key e883dffd-b644-4431-989d-0181ae34d0e6
