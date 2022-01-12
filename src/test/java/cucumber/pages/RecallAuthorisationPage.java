0afaec5607e4f80c11eae322c8a8ac4e3677c540
Jon Wyatt <jonnywyatt2@gmail.com>
2022-01-12 09:38:03 +0000

    PUD-1193 - remove serenity (#148)
    * Run cypress in local-check
    
    * Don't use cache in local-check
    
    * Turn off cypress debug in local-check
    
    * restore_cache
    
    * Use executor in cache key
    
    * Use arch in cache key
    
    * Use chrome browser
    
    * Remove build_docker job
    
    * Delete Serenity
    
    * Note to change noms number
    
    * - Add npm install to build.sh
    - Add comment to readme about running cypress headless/headful
    
    Co-authored-by: Jon Wyatt <>
    Co-authored-by: Phil Segal <phil@phisec.co.uk>

--- src/test/java/cucumber/pages/RecallAuthorisationPage.java
+++
@@ -1,9 +0,0 @@
-package cucumber.pages;
-
-import net.serenitybdd.screenplay.targets.Target;
-
-public class RecallAuthorisationPage extends PpudPage {
-
-    public static final String TITLE = TITLE_ROOT + "Recall assessed";
-    public static final Target RECALL_ID = getTargetByDataQa("recallId");
-}
--- src/test/java/cucumber/pages/ViewRecallPage.java
+++
@@ -1,6 +0,0 @@
-package cucumber.pages;
-
-public class ViewRecallPage extends RecallCheckAnswersPage {
-
-    public static final String TITLE = TITLE_ROOT + "View the recall";
-}
