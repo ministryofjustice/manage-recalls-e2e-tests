cc1b94f717bc17602123dde92dd3c6eba721ed2f
Jon Wyatt <>
2022-01-17 09:48:24 +0000

    Verify change history user and date

--- cypress/integration/manageRecall/changehistory.js
+++ cypress/integration/manageRecall/changehistory.js
@@ -1,9 +1,14 @@
 import {When} from "cypress-cucumber-preprocessor/steps";
-import {recall, caseworker, nomsNumber} from "../../fixtures";
+import {caseworker} from "../../fixtures";
 
 When('Maria navigates to view the change history overview for the recall', () => {
     cy.clickLink('View change history')
     cy.pageHeading().should('equal', 'Change history')
+    cy.getRowValuesFromTable({ parent: '#table-info-entered', rowQaAttr: 'currentPrison' }).then(rowValues => {
+        expect(rowValues[0]).to.equal('Prison held in')
+        expect(rowValues[1]).to.not.be.empty // updated date
+        expect(rowValues[2]).to.equal('Maria Badger')
+    })
 })
 
 When('Maria navigates to view the change history details page for the current prison field for the recall', () => {
