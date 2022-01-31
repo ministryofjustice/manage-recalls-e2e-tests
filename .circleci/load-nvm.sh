38c4aae2924ba5dd073dcfacfd0cef73767997dc
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-01-31 11:24:57 +0000

    Fix CI - I hope...

--- .circleci/load-nvm.sh
+++ .circleci/load-nvm.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-export NVM_DIR="/opt/circleci/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+node_version=$(jq -r '.engines.node' <package.json)
+nvm use $node_version
