19d9f47c48679af1c6e78e9e2139b586e3b57ba8
Darren Oakley <daz.oakley@gmail.com>
2022-02-11 15:30:56 +0000

    PUD-1468: Rescind recall test

---
+++ cypress/integration/stepDefinitions/rescind.js
@@ -0,0 +1,28 @@
+import {When} from "cypress-cucumber-preprocessor/steps";
+
+When('Maria goes to the view recall page', () => {
+    cy.clickLink('Recalls')
+    cy.get('@recallId').then(recallId => {
+        cy.clickButton({qaAttr: `assess-recall-${recallId}`})
+    })
+    cy.get('@firstLastName').then((firstLastName) =>
+        cy.pageHeading().should('equal', `Assess a recall for ${firstLastName}`)
+    )
+})
+
+When('Maria rescinds the recall', () => {
+    cy.clickButton('Actions')
+    cy.clickLink('Rescind recall')
+    cy.pageHeading().should('equal', 'Record a rescind request')
+    cy.fillInput('Provide details about the rescind request', 'blah blah blah')
+    cy.clickButton('Today')
+    cy.uploadEmail({ field: 'rescindRequestEmailFileName', file: 'email.msg' })
+    cy.clickButton('Save and return')
+})
+
+When('Maria checks that the recall has been rescinded', () => {
+    cy.clickLink('View')
+    cy.getText('recallStatus').should('equal', 'Rescind in progress')
+    cy.recallInfo('Rescind request details').should('equal', 'blah blah blah')
+    cy.recallInfo('Rescind request email').should('equal', 'email.msg')
+})
