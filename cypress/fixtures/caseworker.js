0d1580b4efdff201e9c0627164c04e71acf04102
Jon Wyatt <>
2022-03-16 08:46:48 +0000

    Use valid phone number

--- cypress/fixtures/caseworker.js
+++ cypress/fixtures/caseworker.js
@@ -2,5 +2,5 @@ export const caseworker = {
     "firstName": "Maria",
     "lastName": "Badger",
     "email": "maria.badger@thebadgers.org",
-    "phoneNumber": "09876543210"
+    "phoneNumber": "01277563421"
 }
